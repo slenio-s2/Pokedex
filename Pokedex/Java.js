9decf4bb96136e94fc8247ffaf8e4d66191d4605
Hc_Cruz <124739200+slenio-s2@users.noreply.github.com>
2025-08-26 17:02:56 -0300

    Add files via upload

--- Pokedex/Java.js
+++ Pokedex/Java.js
@@ -1,64 +1,61 @@
-const pokeconteiner = document.querySelector("#pokeconteiner");
-const pokemonCount = 150
+const pokeContainer = document.querySelector("#pokeContainer");
+const pokemonCount = 120;
 const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-    electric: '#FCF7DE',
-    water: '#DEF3FD',
-    ground: '#f4e7da',
-    rock: '#d5d5d4',
-    fairy: '#fceaff',
-    poison: '#98d7a5',
-    bug: '#f8d5a3',
-    dragon: '#97b3e6',
-    psychic: '#eaeda1',
-    flying: '#F5F5F5',
-    fighting: '#E6E0D4',
-    normal: '#F5F5F5'
-}
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
 
 const mainTypes = Object.keys(colors);
 
 const fetchPokemons = async () => {
-    for (let i = 1; i <= pokemonCount; i++) {
-        await getPokemons(i)
-    }
-}
+  for (let i = 1; i <= pokemonCount; i++) {
+    await getPokemons(i);
+  }
+};
 
+const getPokemons = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const resp = await fetch(url);
+  const data = await resp.json();
+  createPokemonCard(data);
+};
 
-const getPokemons = async () => {
-    const url = 'https://pokeapi.co/api/v2/pokemon/'
-    const resp = await fetch(url)
-    const data = await resp.json()
-    createpokemoncard(data)
-}
+const createPokemonCard = (poke) => {
+  const card = document.createElement("div");
+  card.classList.add("pokemon");
 
-const createpokemoncard = (poke) => {
-    const card = document.createElement('div')
-    card.classList.add("pokemon")
+  const name = poke.name[0].toUpperCase() + poke.name.slice(1);
+  const id = poke.id.toString().padStart(3, "0");
 
-    const name = poke.name[1].toUpperCase() + poke.name.slice(1)
-    const id = poke.id.toString().padstart(3, '0')
+  const poketypes = poke.types.map((type) => type.type.name);
+  const type = mainTypes.find((type) => poketypes.indexOf(type) > -1);
+  const color = colors[type];
 
-    const poketypes = poke.types.map(type => type.tupe.name)
-    const type = mainTypes.find(type => poketypes.idexof(type) > -1)
-    const color = colors[type]
+  card.style.backgroundcolor = color;
 
-    card.style.backgroundcolor = color
-
-    const pokemoninnerHTML = `  <div class="imagemconteiner">
+  const pokemoninnerHTML = `  <div class="imagemconteiner">
                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png" alt="${name}">
             </div>
 
             <div class="info">
                 <span class="numero">#${id}</span>
-                <h3 class="${name}">bulbasaur</h3>
+                <h3 class="name">${name}</h3>
                 <smal class="tipo">${type}</smal>
-            </div>`
-    card.innerHTML = pokemoninnerHTML
+            </div>`;
+  card.innerHTML = pokemoninnerHTML;
 
-    pokeconteiner.appendchild(card)
-
-    
-}
-fetchPokemons()
+  pokeContainer.appendChild(card);
+};
+fetchPokemons();
